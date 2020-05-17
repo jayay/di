bdccd5f1406dba1ae1c9cd176b70955757687d29
jayay <meine-adresse@live.de>
2020-05-17 17:53:56 +0200

    Put interface into stub

--- di.stub.php
+++ di.stub.php
@@ -7,4 +7,10 @@ class DIContainer implements DIContainerInterface
     public function withClassMap(array $mapping) : DIContainerInterface {}
 }
 
+interface DIContainerInterface {
+    public function get(string $className);
+    public function withInstances(array $instances) : DIContainerInterface;
+    public function withClassMap(array $mapping) : DIContainerInterface;
+}
+
 class DIException extends Exception {}
